58dd5e324bc8eac9c335b56697ce2aaec79a80b2
Zach <itsaboy3366@gmail.com>
2023-08-04 05:19:21 -0400

    hotfix: the alert that was supposed to trigger when no predefined characters were selected would trigger if only uppercase letters were selected - fixed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -141,7 +141,7 @@ const userPrompts = () => {
   criteriaSet = true;
 
   // Prompts user that no character selection criteria were confirmed
-  if (includeLowercase === false && includeLowercase === false && includeSpecial === false && includeNumbers === false) {
+  if (includeLowercase === false && includeUppercase === false && includeSpecial === false && includeNumbers === false) {
     alert("No characters selected! Cannot generate password! Refresh the page to try again!");
     return;
   // Generates password according to user criteria
